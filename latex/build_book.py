1dd90e6707d8ca0db774792f55f6686043ca34f9
Bernie Hogan <bernie.hogan@oii.ox.ac.uk>
2022-07-13 18:55:37 +0100

    reorder and tweak files (to rebuild PDF)

--- latex/build_book.py
+++ latex/build_book.py
@@ -1,26 +1,12 @@
-# check within each subdirectory for the single ipynb file. 
-
-# Use nbconvert to convert to .tex 
-# strip out the preambles 
-# add a single preamble to a main text file 
-# run the build script on the file. 
-
 import glob
 import os
 from pathlib import Path 
 from os import sep as ossep
 
-# Because it seems that multi-character emoji finding in Python is still 
-# not good enough. So this list gets edited by hand and should be equal to
-# or a superset of the total emojis in the book. 
+def ignore_colab(text):
 
-# The emojis have to be sorted so that find and replacing a single character
-# doesn't mess up the multicharacter ones. Luckily we don't have to contend with
-# multicharacter skin tones, but at that point we simple must build a multi-emoji
-# parser. 
-
-
-
+    return text
+    
 def convert_notebooks(pattern="*",output_dir="."):
     
     for file in glob.glob(pattern):
@@ -33,12 +19,14 @@ def main(nbconvert=True,texclean=True,makelatex=True,jobname="book"):
     
     if nbconvert: 
         print("NBCONVERT - Started.")
-        convert_notebooks("./chapters/Ch.*.ipynb","./tex/chapters/")
+        convert_notebooks("../chapters/Ch.*.ipynb","tex/")
+        convert_notebooks("../exercises/Ch.*.ipynb","tex/")
+        
         print("NBCONVERT - Done.")
 
     if texclean:
     
-        for file in glob.glob("./tex/chapters/Ch.*.tex"):
+        for file in glob.glob("tex/Ch.*.tex"):
             blob = open(file).read()
             
             blob_split = blob.split("maketitle",1)
@@ -52,13 +40,15 @@ def main(nbconvert=True,texclean=True,makelatex=True,jobname="book"):
 
         
     if makelatex: 
-        os.chdir("tex/chapters/")
+        os.chdir("tex/")
         os.system(f'xelatex -jobname {jobname} book.tex')
         os.system(f'biber -jobname {jobname} book.tex')
         os.system(f'xelatex -jobname {jobname} book.tex')
         os.system(f'xelatex -jobname {jobname} book.tex')
 
 if __name__ == "__main__":
-#     1/0
-    main(nbconvert=False,texclean=True,jobname = "IntroducingPython")
+    main(nbconvert=True,
+         texclean=True,
+         makelatex=True,
+         jobname = "IntroducingPython")
  
